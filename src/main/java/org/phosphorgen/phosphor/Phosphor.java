47ec09e9a9ae1700b669e10caacbd4ad7c0b7f05
King Rat <king.rattington@gmail.com>
2012-03-04 04:42:32 +0000

    inital commit

---
+++ src/main/java/org/phosphorgen/phosphor/Phosphor.java
@@ -0,0 +1,12 @@
+package org.phosphorgen.phosphor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Phosphor extends JavaPlugin {
+    public void onDisable() {
+    }
+
+    public void onEnable() {
+    }
+}
+
